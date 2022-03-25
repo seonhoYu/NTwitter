460f4ceaee30ad70a73956b4cbdf65f4c73807cc
Seonho Yu <carlos861209@gmail.com>
2022-03-25 23:36:14 +0900

    firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBcZ4IbnApqTP9vOxDaLlaYlC1XYqYwIzk",
+    authDomain: "ntwitter-55b42.firebaseapp.com",
+    projectId: "ntwitter-55b42",
+    storageBucket: "ntwitter-55b42.appspot.com",
+    messagingSenderId: "868132597833",
+    appId: "1:868132597833:web:1d3a3b0156406d23b80f98"
+  };
+
+ export default firebase.initializeApp(firebaseConfig);
